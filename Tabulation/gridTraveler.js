7707aaa7804a558651f13ac7b6d0991b94b9abcb
Anderson Ochoa <aaochoa4@gmail.com>
2022-07-15 14:23:20 -0500

    Tabulated Grid taveler

---
+++ Tabulation/gridTraveler.js
@@ -0,0 +1,22 @@
+// Tabulated GridTraveler
+const tabulatedGridTraveler = (m, n) => {
+  const table = Array(m+1).fill().map(() => Array(n+1).fill(0));
+  table[1][1] = 1;
+  for (let i = 0; i <= m; i++) {
+    for(let j = 0; j <= n; j++) {
+      const current = table[i][j];
+      if (i+1 <= m) table[i+1][j] += current;
+      if (j+1 <= n) table[i][j+1] += current;
+    }
+  }
+  return table[m][n];
+};
+
+console.time('Tabulated GridTraveler');
+console.log(tabulatedGridTraveler(1,1));
+console.log(tabulatedGridTraveler(2,3));
+console.log(tabulatedGridTraveler(3,1));
+console.log(tabulatedGridTraveler(3,3));
+console.log(tabulatedGridTraveler(10,10));
+console.log(tabulatedGridTraveler(16,16));
+console.timeEnd('Tabulated GridTraveler');
